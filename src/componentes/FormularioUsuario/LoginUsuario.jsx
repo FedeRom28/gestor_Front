b34bf23de926e894e38281c8970a754b1c0fbbd8
federomero <federom@gmail.com>
2025-08-28 00:26:14 -0300

    osoow

--- src/componentes/FormularioUsuario/LoginUsuario.jsx
+++ src/componentes/FormularioUsuario/LoginUsuario.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-import jwtDecode from "jwt-decode"; // ✅ Import correcto
+import { jwtDecode } from "jwt-decode"; // ✅ Import nombrado
 
 class LoginUsuario extends Component {
   constructor(props) {
